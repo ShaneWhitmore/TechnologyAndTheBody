131d91b82e35da3758b2c4b7796ef1897c50d309
Shane Whitmore <113622023+ShaneWhitmore@users.noreply.github.com>
2025-01-27 09:19:36 +0000

    Updated 'Prosthetic.ino'
    Code has been compiled but not implemented, Servo created + initial code for closing and opening the hand

--- Semester 2/Implemented Programs/Prosthetic/Prosthetic.ino
+++ Semester 2/Implemented Programs/Prosthetic/Prosthetic.ino
@@ -1,5 +1,18 @@
 #include "ESP32_NOW.h"
 #include "WiFi.h"
+#include <ESP32Servo.h>
+
+
+Servo pointerServo;
+Servo middleServo;
+Servo ringServo;
+Servo pinkyServo;
+
+int pointerPin = 13;
+int middlePin = 18;
+int ringPin = 17;
+int pinkyPin = 16;
+
 
 
 //Variables
@@ -13,6 +26,23 @@ void onDataRecv(const esp_now_recv_info_t *recvInfo, const uint8_t *data, int da
   Serial.println(receivedValue);
 }
 
+void closeHand()
+{
+  middleServo.write(70);
+  delay(400);
+  middleServo.write(90);
+}
+
+
+void openHand()
+{
+  middleServo.write(100);
+  delay(400);
+  middleServo.write(90);
+}
+
+
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -21,11 +51,26 @@ void setup() {
 
   esp_now_register_recv_cb(onDataRecv);
 
+  pointerServo.attach(pointerPin);
+  middleServo.attach(middlePin);
+  ringServo.attach(ringPin);
+  pinkyServo.attach(pinkyPin);
+  
+
+
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   delay(100);
+
+  //recieve muscle state
+  // check current state (false = open hand , true = closed hand)
+  // if receivedState && !currentState
+  // close over the hand -> make this a function
+  // else if !receivedState && currentState 
+  // open the hand -> make this a function
+
 
 }
