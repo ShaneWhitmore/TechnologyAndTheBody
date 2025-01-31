8e227167ebad646bdae5bda0f321768ae3fac1bb
Shane Whitmore <113622023+ShaneWhitmore@users.noreply.github.com>
2025-01-31 10:52:48 +0000

    Prosthetic + Wearable Update
    Latest version reads and interperates the MyoWare signal to a boolean value,
    
    Boolean value + Signal value is sent using esp-now.
    
    Prosthetic receives signal and enters the appropriate function to open/close hand. Issue occurs where servos will not close , this may be due to many esp-now messages coming in to frequently.

--- Semester 2/Implemented Programs/Prosthetic/Prosthetic.ino
+++ Semester 2/Implemented Programs/Prosthetic/Prosthetic.ino
@@ -8,37 +8,85 @@ Servo middleServo;
 Servo ringServo;
 Servo pinkyServo;
 
-int pointerPin = 13;
-int middlePin = 18;
-int ringPin = 17;
+int pointerPin = 14;
+int middlePin = 17;
+int ringPin = 18;
 int pinkyPin = 16;
 
+// available pins for servos 
+/*
+ADC1_CH1 (GPIO 37)
+ADC1_CH2 (GPIO 38)
+ADC1_CH3 (GPIO 39)
+ADC1_CH4 (GPIO 32)
+ADC1_CH5 (GPIO 33)
+ADC1_CH6 (GPIO 34)
+ADC1_CH7 (GPIO 35)
+
+GPIO 13, 14, 15, 16, 17, 18, 19, 21, 22, 23, 25, 26, 27
+
+*/
+
+
+typedef struct struct_message {
+  bool t;
+  uint16_t d; 
+} struct_message;
+
+struct_message receivedData;
 
 
 //Variables
 uint16_t receivedValue;
+int currentState = 0;
 
 
 // Callback when data is received
 void onDataRecv(const esp_now_recv_info_t *recvInfo, const uint8_t *data, int dataLen) {
-  memcpy(&receivedValue, data, sizeof(receivedValue));
+  memcpy(&receivedData, data, sizeof(receivedData));
   Serial.print("Muscle State: ");
-  Serial.println(receivedValue);
+  Serial.println(receivedData.d);
+  Serial.println(receivedData.t);
+
+  if(receivedData.t == 1)
+  {
+    Serial.println("Closing Hand");
+    closeHand();
+  }
+  else
+  {
+    Serial.println("Opening Hand");
+    openHand();
+  }
+
 }
 
 void closeHand()
 {
-  middleServo.write(70);
-  delay(400);
-  middleServo.write(90);
+  if(receivedData.t != currentState)
+  {
+    Serial.println("YAYYYYYY");
+
+    middleServo.write(50);
+    delay(400);
+    middleServo.write(90);
+    currentState = receivedData.t;
+  }  
 }
 
 
 void openHand()
 {
-  middleServo.write(100);
-  delay(400);
-  middleServo.write(90);
+  if(receivedData.t != currentState)
+  {
+
+    Serial.println("YAYYYYYY2222222");
+
+    middleServo.write(160);
+    delay(400);
+    middleServo.write(90);
+    currentState = receivedData.t;
+  }
 }
 
 
@@ -47,6 +95,18 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   WiFi.mode(WIFI_STA);
+
+  
+  while (!WiFi.STA.started()) {
+    delay(100);
+  }
+
+
+  if (esp_now_init() != ESP_OK)
+  {
+    Serial.print("ESP NOW Initialisation Error");
+    return;
+  }
 
 
   esp_now_register_recv_cb(onDataRecv);
--- Semester 2/Implemented Programs/Wearable/Wearable.ino
+++ Semester 2/Implemented Programs/Wearable/Wearable.ino
@@ -1,25 +1,28 @@
 #include "ESP32_NOW.h"
 #include "WiFi.h"
-
+//devices mac address = 24:0a:c4:2e:57:64
 
 //Global variables
 uint16_t sensorValue;
 int pinENV = 33;
+int maxVal = 4095;
+bool muscleState = false;
 
 //Prosthetic mac address
-uint8_t broadcastAddress[] = {0x88, 0x13, 0xBF , 0x68 , 0xB2 , 0x8C}; //88:13:BF:68:B2:8C
+uint8_t broadcastAddress[] = { 0x88, 0x13, 0xBF, 0x68, 0xB2, 0x8C };  //88:13:BF:68:B2:8C
+
 
 
 //message structure
 typedef struct struct_message {
-    int d;
+  bool t;
+  uint16_t d;
 } struct_message;
 
 struct_message myData;
 
 
-void OnDataSent(const uint8_t *mac_addr , esp_now_send_status_t status)
-{
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
   Serial.print("\r\nLast Packet Send Status:\t");
   Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
 }
@@ -34,15 +37,25 @@ void setup() {
     delay(100);
   }
 
+  if (esp_now_init() != ESP_OK) {
+    Serial.print("Initialising ESP NOW");
+    return;
+  }
+
+
   esp_now_register_send_cb(OnDataSent);
 
 
   //Register receiver mac address
   esp_now_peer_info_t peerInfo;
   memcpy(peerInfo.peer_addr, broadcastAddress, 6);
-  peerInfo.channel = 0;  
+  peerInfo.channel = 0;
   peerInfo.encrypt = false;
 
+  if (esp_now_add_peer(&peerInfo) != ESP_OK) {
+    Serial.println("Failed to add peer");
+    return;
+  }
 }
 
 void loop() {
@@ -50,9 +63,34 @@ void loop() {
 
   //Read from sensor
   int sensorValue = analogRead(pinENV);
+  Serial.println(sensorValue);
+  // Received from: 24:0A:C4:2E:57:64 | Muscle State: 4095 , maximum value can be 4095
+
+
+
+
+
+
+  //interperate sensor data
+
+
+  //maxVal = 4095
+  float upperLimit = maxVal * 0.75;
+  float lowerLimit = maxVal * 0.25;
+
+  if (sensorValue > upperLimit) {
+    muscleState = true;
+  } else if (sensorValue < lowerLimit) {
+    muscleState = false;
+  }
+
+  myData.t = muscleState;
   myData.d = sensorValue;
 
-  //broadcast sensor value
+
+
+
+  //broadcast muscle stimulation state
   esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *)&myData, sizeof(myData));
 
   if (result == ESP_OK) {
@@ -61,15 +99,5 @@ void loop() {
     Serial.println("Error sending message");
   }
 
-  delay(100);
-
-  //interperate sensor data
-
-
-
-
-
-  //broadcast muscle stimulation state 
-
-
+  delay(1000);
 }
